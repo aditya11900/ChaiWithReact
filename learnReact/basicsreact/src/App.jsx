da3d39bcf7286adab8abb9dea4ce3a68e25b5466
aditya11900 <adityaraj153566@gmail.com>
2024-05-31 15:52:21 +0530

    dealing with files

---
+++ learnReact/basicsreact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from './chai'
+function App() {
+
+  return (
+    <>
+    <Chai />
+    <h1>hello g toh kaise hai aaplog</h1>
+    </>
+  )
+}
+
+export default App
---
+++ learnReact/basicsreact/src/chai.jsx
@@ -0,0 +1,20 @@
+function Chai(){
+    return(
+        <div>
+            <h1>Chai</h1>
+        </div>
+    )
+}
+
+export default Chai;
+
+//we have to return only one element in the return statement
+//so we have to wrap all the elements in a div tag or a fragment tag
+// {<>
+//<h1>Chai</h1>
+//<p>Chai is a drink</p>
+// <> }
+
+//convention always try to make the component name in capital letter
+//and the file name should also be in capital letter
+//this is a convention to follow
